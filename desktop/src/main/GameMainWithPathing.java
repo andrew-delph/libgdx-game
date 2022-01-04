b657fc834892f4370d19ed3ecba9c802c380280f
Andrew Delph <andrewdelph@live.ca>
2022-01-03 21:22:26 -0500

    GameMainWithPathing

---
+++ desktop/src/main/GameMainWithPathing.java
@@ -0,0 +1,24 @@
+package main;
+
+import app.GameScreen;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import configuration.SoloConfig;
+import entity.pathfinding.EdgeRegistration;
+
+public class GameMainWithPathing {
+    public static void main(String[] arg) {
+        Injector injector = Guice.createInjector(new SoloConfig());
+        GameScreen gameScreen = injector.getInstance(GameScreen.class);
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+        EdgeRegistration edgeRegistration = injector.getInstance(EdgeRegistration.class);
+        edgeRegistration.edgeRegistration();
+
+        config.height = 500;
+        config.width = 500;
+        new LwjglApplication(gameScreen, config);
+    }
+}
