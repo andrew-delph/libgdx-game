cdda3e595e0fa5bb5932d754f08b18ed17d28ff4
Andrew Delph <andrewdelph@live.ca>
2022-02-05 09:42:03 -0500

    add no pathing server

--- desktop/src/main/ServerRunnerWithPathing.java
+++ desktop/src/main/ServerRunnerWithPathing.java
@@ -4,15 +4,14 @@ import app.game.Game;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import common.exceptions.SerializationDataMissing;
-import configuration.BaseServerConfig;
 import configuration.MainBaseServerConfig;
 
 import java.io.IOException;
 
-public class ServerRunner {
+public class ServerRunnerWithPathing {
 
     public static void main(String[] args) throws IOException, InterruptedException, SerializationDataMissing {
-        Injector injector = Guice.createInjector(new BaseServerConfig());
+        Injector injector = Guice.createInjector(new MainBaseServerConfig());
         Game game = injector.getInstance(Game.class);
         game.start();
 
