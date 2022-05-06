8af6c8c4d1f29c066b9b62260552f8aff21af11c
Andrew Delph <andrewdelph@live.ca>
2022-05-06 10:15:21 -0400

    diff 2

--- desktop/src/main/Test.java
+++ desktop/src/main/Test.java
@@ -3,7 +3,7 @@ package main;
 public class Test {
 
   public static void main(String[] args) throws InterruptedException {
-    // diff
+    // diff 2
     Long x = 100l;
     x = (long) (x * 1.11111111111111111111);
     System.out.println(x);
