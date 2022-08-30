af0c6425019cd17ddb8fc43b2754b786f7323ff9
Andrew Delph <andrewdelph@live.ca>
2022-08-30 12:40:26 -0400

    sand cannot move on x axis

--- core/src/core/entity/controllers/SandController.java
+++ core/src/core/entity/controllers/SandController.java
@@ -1,6 +1,10 @@
 package core.entity.controllers;
 
+import com.badlogic.gdx.math.Vector2;
 import core.app.game.GameController;
+import core.common.CommonFactory;
+import core.common.Coordinates;
+import core.common.GameSettings;
 import core.common.GameStore;
 import core.common.events.EventService;
 import core.entity.Entity;
@@ -17,5 +21,25 @@ public class SandController extends EntityController {
       EventTypeFactory eventTypeFactory,
       Entity entity) {
     super(gameController, gameStore, entityActionFactory, eventService, eventTypeFactory, entity);
+  }
+
+  @Override
+  public void beforeWorldUpdate() throws Exception {
+    super.beforeWorldUpdate();
+  }
+
+  @Override
+  public void afterWorldUpdate() throws Exception {
+
+    Coordinates moveTo =
+        CommonFactory.createCoordinates(
+            this.entity.getCoordinatesWrapper().getCoordinates().getX(),
+            this.entity.getBodyPosition().y / GameSettings.PHYSICS_SCALE);
+
+    if (!this.entity.getCoordinatesWrapper().getCoordinates().equals(moveTo))
+      gameController.moveEntity(this.entity.getUuid(), moveTo);
+
+    entity.setBodyPosition(entity.getCoordinatesWrapper().getCoordinates().toPhysicsVector2());
+    entity.setBodyVelocity(new Vector2(0, entity.getBodyVelocity().y));
   }
 }
