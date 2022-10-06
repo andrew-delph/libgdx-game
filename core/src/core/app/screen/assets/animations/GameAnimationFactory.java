e6be66fd36cd3cbbe9c143ba7fc680abd27d1615
Andrew Delph <andrewdelph@live.ca>
2022-10-06 08:50:07 -0400

    add my sprite sheets

--- core/src/core/app/screen/assets/animations/GameAnimationFactory.java
+++ core/src/core/app/screen/assets/animations/GameAnimationFactory.java
@@ -7,7 +7,7 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class GameAnimationFactory {
 
-  GameAnimation createEntityAnimation() {
+  GameAnimation createEntityAnimation2() {
     final int FRAME_COLS = 6, FRAME_ROWS = 5;
     Texture walkSheet = new Texture(Gdx.files.internal("sprite-animation4.png"));
     TextureRegion[][] tmp =
@@ -42,6 +42,72 @@ public class GameAnimationFactory {
             AnimationState.WALKING_RIGHT, new Animation<TextureRegion>(0.025f, walkRightFrames))
         .addAnimation(
             AnimationState.WALKING_LEFT, new Animation<TextureRegion>(0.025f, walkLeftFrames))
+        .build();
+  }
+
+  GameAnimation createEntityAnimation() {
+    final int DEFAULT_FRAMES_NUM = 23;
+    final int DEFAULT_FRAME_COLS = 6, DEFAULT_FRAME_ROWS = 4;
+    final String DEFAULT_FILE_NAME = "default_spritesheet2.png";
+    Texture walkSheet = new Texture(Gdx.files.internal(DEFAULT_FILE_NAME));
+    TextureRegion[][] tmp =
+        TextureRegion.split(
+            walkSheet,
+            walkSheet.getWidth() / DEFAULT_FRAME_COLS,
+            walkSheet.getHeight() / DEFAULT_FRAME_ROWS);
+
+    TextureRegion[] defaultFrames = new TextureRegion[DEFAULT_FRAMES_NUM];
+    int index = 0;
+    for (int i = 0; i < DEFAULT_FRAME_ROWS; i++) {
+      for (int j = 0; j < DEFAULT_FRAME_COLS; j++) {
+        if (index >= DEFAULT_FRAMES_NUM) break;
+        defaultFrames[index++] = tmp[i][j];
+      }
+    }
+
+    final int WALKING_FRAMES_NUM = 26;
+    final int WALKING_FRAME_COLS = 6, WALKING_FRAME_ROWS = 5;
+    final String WALKING_FILE_NAME = "walking_spritesheet.png";
+
+    walkSheet = new Texture(Gdx.files.internal(WALKING_FILE_NAME));
+    tmp =
+        TextureRegion.split(
+            walkSheet,
+            walkSheet.getWidth() / WALKING_FRAME_COLS,
+            walkSheet.getHeight() / WALKING_FRAME_ROWS);
+
+    TextureRegion[] walkLeftFrames = new TextureRegion[WALKING_FRAMES_NUM];
+    index = 0;
+    for (int i = 0; i < WALKING_FRAME_ROWS; i++) {
+      for (int j = 0; j < WALKING_FRAME_COLS; j++) {
+        //        tmp[i][j].flip(true, false);
+        if (index >= WALKING_FRAMES_NUM) break;
+
+        walkLeftFrames[index++] = tmp[i][j];
+      }
+    }
+
+    TextureRegion[] walkRightFrames = new TextureRegion[WALKING_FRAMES_NUM];
+    tmp =
+        TextureRegion.split(
+            walkSheet,
+            walkSheet.getWidth() / WALKING_FRAME_COLS,
+            walkSheet.getHeight() / WALKING_FRAME_ROWS);
+    index = 0;
+    for (int i = 0; i < WALKING_FRAME_ROWS; i++) {
+      for (int j = 0; j < WALKING_FRAME_COLS; j++) {
+        if (index >= WALKING_FRAMES_NUM) break;
+        tmp[i][j].flip(true, false);
+        walkRightFrames[index++] = tmp[i][j];
+      }
+    }
+
+    return new GameAnimationBuilder()
+        .addAnimation(AnimationState.DEFAULT, new Animation<TextureRegion>(0.25f, defaultFrames))
+        .addAnimation(
+            AnimationState.WALKING_LEFT, new Animation<TextureRegion>(0.05f, walkLeftFrames))
+        .addAnimation(
+            AnimationState.WALKING_RIGHT, new Animation<TextureRegion>(0.05f, walkRightFrames))
         .build();
   }
 
