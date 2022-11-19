71824a4b4aa80bd92b6aadb7f05b5109642ff5e1
Andrew Delph <andrewdelph@live.ca>
2022-11-19 11:06:13 -0500

    create digging right and digging left states

---
+++ core/src/core/entity/statemachine/states/DiggingRightState.java
@@ -0,0 +1,64 @@
+package core.entity.statemachine.states;
+
+import core.app.screen.assets.animations.AnimationState;
+import core.common.Coordinates;
+import core.common.exceptions.EntityNotFound;
+import core.entity.Entity;
+import core.entity.attributes.msc.AnimationStateWrapper;
+import core.entity.attributes.msc.Health;
+import core.entity.block.Block;
+import core.entity.block.SolidBlock;
+import core.entity.collision.RayCastService;
+import core.entity.statemachine.EntityStateMachineNodeInterface;
+import java.util.Set;
+
+public class DiggingRightState extends EntityStateMachineNodeInterface {
+
+  RayCastService rayCastService;
+
+  public DiggingRightState(RayCastService rayCastService) {
+    this.rayCastService = rayCastService;
+  }
+
+  @Override
+  public void callAnimation(Entity entity) {
+    if (!entity
+        .getAnimationStateWrapper()
+        .getAnimationState()
+        .equals(AnimationState.DIGGING_RIGHT)) {
+      try {
+        gameController.updateEntityAttribute(
+            entity.getUuid(), new AnimationStateWrapper(AnimationState.DIGGING_RIGHT));
+      } catch (EntityNotFound e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  @Override
+  public void callAction(Entity entity, long timeInState) {
+
+    if (timeInState < 150) return;
+    // after time
+
+    Coordinates entityCoordinates = entity.getCoordinatesWrapper().getCoordinates();
+    Coordinates targetCoordinates = entityCoordinates.add(-1, 0);
+
+    Set<Entity> rayCastSet = rayCastService.rayCast(entityCoordinates, targetCoordinates);
+
+    Block targetBlock =
+        (Block) (rayCastSet.stream().filter(e -> e instanceof SolidBlock).findAny()).orElse(null);
+
+    if (targetBlock == null) return;
+
+    Health newHealth = targetBlock.getHealth().applyDiff(-20);
+
+    try {
+      gameController.updateEntityAttribute(targetBlock.getUuid(), newHealth);
+    } catch (EntityNotFound e) {
+      e.printStackTrace();
+    }
+
+    entity.getEntityStateMachine().setState(AnimationState.DEFAULT);
+  }
+}
